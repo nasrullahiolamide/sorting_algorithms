2731e6c5d7f01cfeab0b963c97b8bcef24678112
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-10-12 05:47:20 +0100

    sort.h : betty

--- sort.h
+++ sort.h
@@ -1,8 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -12,18 +10,15 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void swap(int *a, int *b);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
-
-
-#endif  /* SORT_H */
+#endif
